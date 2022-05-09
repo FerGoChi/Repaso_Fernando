7015eb54e70e2ff20d6e064a2806e5b9690d0f2c
ALUMNOS <ALUMNOS@P606>
2022-05-09 09:12:06 +0200

    Primer commit

---
+++ Repaso/src/Repaso/TablaMultiplicar.java
@@ -0,0 +1,18 @@
+package Repaso;
+public class TablaMultiplicar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+
+			for (int tabla=1;tabla<=10;tabla++) {
+				System.out.println("\\tabla.del."+tabla);
+					
+					for (int num=1;num<=10;num++) {
+						System.out.println(tabla+ " X " + num + " = "+ (tabla*num));
+										
+					}
+					
+		}
+	}
+}
